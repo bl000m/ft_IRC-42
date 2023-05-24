8e572872448fe5aee1ca823af84c011c12275a62
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-24 22:24:25 +0200

    wip

--- Message.cpp
+++ Message.cpp
@@ -56,13 +56,99 @@ Message	&Message::operator=(Message const &mess)
 	return (*this);
 }
 
+/*
+	return true when parsing succeed
+	return false when parsing fails (ex. empty message, in this case, it must be silently ignored)
+*/
 bool	Message::parse(char buffer[512])
 {
-	//check \r\n present
+	std::string		input;
+	std::size_type	pos;
+	
+	this->clear();
+	if (buffer_empty(buffer))
+		return (false);
+	input = read_buffer(buffer);
+	pos = 0;
 	//get source
 	//get cmd
 	//get param
 }
+
+void	Message::setSource(std::string const &input, std::string::size_type &pos)
+{	
+	pos = 0;
+	if (*(input.begin()) != ':')
+		return ;
+	pos = input.find(" ", 0);
+	_src = new input.substr(0, pos);
+	if (pos == std::string::npos)
+		return ;
+	pos = input.find_first_not_of(" ", pos);
+}
+
+void	Message::setCommand(std::string const &input, std::string::size_type &pos)
+{
+	std::string::size_type	end;
+
+	if (pos == std::string::npos)
+		return ;
+	end = input.find(" ", pos);
+	_cmd = new input.substr(pos, end - pos);
+	pos = end;
+	if (pos == std::string::npod)
+		return ;
+	pos = input.find_first_not_of(" ", pos);
+}
+
+void	Message::setParam(std::string const &input, std::string::size_type &pos)
+{
+
+}
+
+
+/*
+	look for \r\n in buffer, and replace it with \0
+	if \r\n is not present, look for \n
+	if even \n is not present, 
+	it acts as if \r\n are at the end of buffer
+*/
+std::string	Message::read_buffer(char buf[512])
+{
+	std::string	temp;
+	int			i;
+	
+	i = 0;
+	while (i < 512)
+	{
+		if (buff[i] == '\n')
+			break ;
+		i++;
+	}
+	if (i == 512)
+		buf[510] = '\0';
+	else if (buff[i - 1] == '\r')
+		buff[i - 1] = '\0';
+	else
+		buff[i] = '\0';
+	temp = buf;
+	return (temp);
+}
+
+/*
+	message is ended with \r\n,
+	if it's ended with a single \n, although incorrect,
+	must be handled as well
+*/
+bool	Message::buffer_empty(char buf[512])
+{
+	if (buf[0] == '\r' && buf[1] == '\n')
+		return (true);
+	if (buf[0] == '\n')
+		return (true);
+	return (false);
+}
+
 
 std::string const	*Message::getSource(void) const
 {
--- Message.hpp
+++ Message.hpp
@@ -10,7 +10,9 @@
 	PS: do not implement TAG
 
 	A message can have at most 15 parameter
-	
+	Every message ends with \r\n, but a simple \n is acceptable
+	If the \r\n is not present, it'a assumed the message is too long. The rest of the info is ignored, and it's treated as if a \r\n is at the end.
+	An empty message is ignored.
 */
 
 # define SIXTEEN 16
@@ -26,16 +28,26 @@ class Message
 
 		bool	parse(char buffer[512]);
 
+		/*	getters	*/
 		std::string const	*getSource(void) const;
 		std::string	const	*getCommand(void) const;
 		std::string const	**getParam(void) const;
 
 	private:
+		/*	private member variable	*/
 		std::string	*_src;
 		std::string	*_cmd;
 		std::string	*_param[SIXTEEN];
 
-		void	clear(void);
+		/*	setters	*/
+		void	setSource(std::string const &input, std::string::size_type &pos);
+		void	setCommand(std::string const &input, std::string::size_type &pos);
+		void	setParam(std::string const &input, std::string::size_type &pos);
+
+		/*	helper function	*/
+		void				clear(void);
+		static std::string	read_buffer(char buf[512]);
+		static bool			buffer_empty(char buf[512]);
 };
 
 std::ostream	&operator<<(ostream &out, Message const &mess);
