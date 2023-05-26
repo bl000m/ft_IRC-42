9af7e9f5158209cf926ed0a36d0a095c97580acd
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-26 10:41:41 +0200

    add member func

--- Client.cpp
+++ Client.cpp
@@ -1,11 +1,13 @@
 #include "Client.hpp"
 
+/*	canon form	*/
 Client::Client(void)
-	:_sock(-1), _ip(0), _pass(false),
+	:_sock(-1), _ip(0), _pass(false), _regist(false),
 	_nick(NULL), _user(NULL), _host(NULL) {}
 
 Client::Client(Client const &client)
-	:_sock(client._sock), _ip(client._ip), _pass(client._pass),
+	:_sock(client._sock), _ip(client._ip),
+	_pass(client._pass), _regist(client._regist)
 	_nick(NULL), _user(NULL), _host(NULL)
 {
 	if (client._nick)
@@ -28,6 +30,7 @@ Client	&Client::operator=(Client const &client)
 	_sock = client._sock;
 	_ip = client._ip;
 	_pass = client._pass;
+	_regist = client._regist;
 	clear();
 	if (client._nick)
 		_nick = new std::string(*client._nick);
@@ -38,24 +41,29 @@ Client	&Client::operator=(Client const &client)
 	return (*this);
 }
 
+/*	ctor with arguments	*/
 Client::Client(int sockfd, unsigned int ip)
-	:_sock(sockfd), _ip(ip), _pass(false),
+	:_sock(sockfd), _ip(ip), _pass(false), _regist(false),
 	_nick(NULL), _user(NULL), _host(NULL) {}
 
 /*
 	the modification of client has certain restrictions,
 	and it is up to the caller to verify that the rules are meet
 	For example,
-	1. Once a client sent a PASS with correct password, 
-		it cannot resend the message.
-	2. The client cannot change nickname to another nickname that is in use
-	3. The username and hostname once set, cannot be changed.
-		Attempt to resend USER command should recieve a correspondant reply
+	1. Server should verify if there's nickname collsion
+	2. Once register, PASS and USER should not be accepted, and should generate an error message.
 */
-void	Client::bePassed(void)
+
+/*	setters	*/
+void	Client::setPass(bool good)
 {
-	_pass = true;
+	_pass = good;
 }
+void	Client::beRegist(void) const
+{
+	_regist = true;
+}
+
 void	Client::setNick(std::string const &nick)
 {
 	delete _nick;
@@ -72,6 +80,23 @@ void	Client::setHost(std::string const &host)
 	_host = new std::string(host);
 }
 
+/*	getters	*/
+int		Client::getSock(void) const
+{
+	return (_sock);
+}
+unsigned int	Client::getIp(void) const
+{
+	return (_ip);
+}
+bool	Client::getPass(void) const
+{
+	return (_pass);
+}
+bool	Client::isRegist(void) const
+{
+	return (_regist);
+}
 std::string const	*Client::getNick(void) const
 {
 	return (_nick);
@@ -84,25 +109,8 @@ std::string const	*Client::getHost(void) const
 {
 	return (_host);
 }
-int		Client::getSock(void) const
-{
-	return (_sock);
-}
-bool	Client::isPassed(void) const
-{
-	return (_pass);
-}
-unsigned int	Client::getIp(void) const
-{
-	return (_ip);
-}
-bool	Client::isRegist(void) const
-{
-	if (_pass && _nick && _user && _host)
-		return (true);
-	return (false);
-}
 
+/*	private function	*/
 void	Client::clear(void)
 {
 	delete _nick;
@@ -113,12 +121,14 @@ void	Client::clear(void)
 	_host = NULL;
 }
 
+/*	opeartor<< overload	*/
 std::ostream	&operator<<(std::ostream &out, Client const &client)
 {
 	out << "Client: "
 		<< "  sock: " << client.getSock() << std::endl
 		<< "  ip: " << client.getIp() << std::endl
 		<< "  pass: " << client.isPassed() << std::endl
+		<< "  regist: " << client.isRegist() << std::endl
 		<< "  nick: ";
 	if (client.getNick())
 		out << *client.getNick() << std::endl;
