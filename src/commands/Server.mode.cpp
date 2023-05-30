d36361b305b4de141d2392e56928f122eaaee707
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-30 23:04:08 +0200

    mode wip

--- src/commands/Server.mode.cpp
+++ src/commands/Server.mode.cpp
@@ -37,12 +37,11 @@ void	mode_user(Client &client, Message const &mess, std::string target)
 	}
 	if (mess.getParamNum() < 2)
 	{
-		 //RPL_UMODEIS : return current mode of the target
-		 return ;
+		reply(client,  RPL_UMODEIS, client.getMode(), NULL);
+		return ;
 	}
 	mode = mess.getParam()[1];
 	//set mode
-	//reply with MODE message
 	// ERR_UMODEUNKNOWNFLAG if there's unknown mode
-
+	reply(client, "MODE", client.getMode(), NULL);	//reply with MODE message
 }
