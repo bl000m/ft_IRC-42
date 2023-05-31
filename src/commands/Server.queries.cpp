12b5eb64f2e23ecbbc1dc716f52bae4d1ed6243c
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-31 14:03:14 +0200

    bugs

--- src/commands/Server.queries.cpp
+++ src/commands/Server.queries.cpp
@@ -17,13 +17,13 @@ void	Server::wallops(Client &client, Message const &mess)
 		reply(client, ERR_NEEDMOREPARAMS, "WALLOPS", ":Not enough parammeter");
 		return ;
 	}
-	if (client.isServerOp())
+	if (!client.isServerOp())
 	{
 		reply(client, ERR_NOPRIVILEGES, ":Permission Denied- You're not an IRC operator", NULL);
 		return ;
 	}
-	note = ":" + client.getFullName() + " WALLOPS "
-			+ mess.getParam()[0];
+	note = ":" + client.getFullName() + " WALLOPS :"
+			+ mess.getParam()[0] + "\r\n";
 	for (i = _clients.begin(); i != _clients.end(); i++)
 	{
 		if (i->second.getWallop())
