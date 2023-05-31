affdd3d784e7190621b1d238b9a9ecf5319f815f
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-31 11:07:29 +0200

    wallops

--- src/commands/Server.queries.cpp
+++ src/commands/Server.queries.cpp
@@ -1,6 +1,35 @@
 #include "Server.hpp"
 
-//motd
+void	Server::motd(Client &client, Message const &mess)
+{
+	(void) client;
+	(void) mess;
+	reply(client,  ERR_NOMOTD, "no MOTD", NULL);
+}
+
+void	Server::wallops(Client &client, Message const &mess)
+{
+	client_map::const_iterator	i;
+	std::string					note;
+	
+	if (mess.getParamNum() < 1)
+	{
+		reply(client, ERR_NEEDMOREPARAMS, "WALLOPS", ":Not enough parammeter");
+		return ;
+	}
+	if (client.isServerOp())
+	{
+		reply(client, ERR_NOPRIVILEGES, ":Permission Denied- You're not an IRC operator", NULL);
+		return ;
+	}
+	note = ":" + client.getFullName() + " WALLOPS "
+			+ *(mess.getParam()[0]);
+	for (i = _clients.begin(); i != _clients.end(); i++)
+	{
+		if (i->second.getWallop())
+			send(i->second->getSock(), note.c_str(), note.size(), 0);
+	}
+}
+
 //who
-//wallop
 //kill
