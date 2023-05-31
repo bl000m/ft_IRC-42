ac402dd706d06e35564c543131f6d212647e9556
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-31 12:58:43 +0200

    wip

--- src/commands/Server.command.cpp
+++ src/commands/Server.command.cpp
@@ -96,8 +96,12 @@ Server::fn_map	Server::cmd_init(void)
 	temp["QUIT"] = &Server::quit;
 	temp["PING"] = &Server::ping;
 	temp["PONG"] = &Server::pong;
-	temp["PRIVMSG"] = &Server::privmsg; 
-	temp["NOTICE"] = &Server::notice; 
+	temp["PRIVMSG"] = &Server::privmsg;
+	temp["NOTICE"] = &Server::notice;
+	temp["MODE"] = &Server::mode;
+	temp["MOTD"] = &Server::motd;
+	temp["WALLOPS"] = &Server::wallops;
+	temp["KILL"] = &Server::kill;
 
 	return (temp);
 }
