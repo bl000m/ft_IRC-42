3894d28ef54b2cf9d78130bf2d7d109b76bb6242
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-31 13:00:19 +0200

    wip

--- src/commands/Server.command.cpp
+++ src/commands/Server.command.cpp
@@ -96,6 +96,7 @@ Server::fn_map	Server::cmd_init(void)
 	temp["QUIT"] = &Server::quit;
 	temp["PING"] = &Server::ping;
 	temp["PONG"] = &Server::pong;
+	temp["OPER"] = &Server::oper;
 	temp["PRIVMSG"] = &Server::privmsg;
 	temp["NOTICE"] = &Server::notice;
 	temp["MODE"] = &Server::mode;
